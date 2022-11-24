e05ab9c521a36e2d4382d619b0d257bed1acc646
Ventsislav Kisyov <ventsislav755@gmail.com>
2022-11-24 10:42:52 +0200

    Solution of inheritance

---
+++ AnimalFarm/Main.java
@@ -0,0 +1,16 @@
+package EncapsulationSoftUni.AnimalFarm;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        String name = scanner.nextLine();
+        int age = Integer.parseInt(scanner.nextLine());
+
+        Chicken chicken = new Chicken(name,age);
+        chicken.toString();
+    }
+}
